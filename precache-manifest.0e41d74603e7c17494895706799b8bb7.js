4b0b0cf3752965b7161324a3962620274e36b539
Sean <seanreilly123@hotmail.com>
2020-06-23 23:27:44 +1200

    Updates

---
+++ precache-manifest.0e41d74603e7c17494895706799b8bb7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c2d3091658d3fd9e2d85cc5f9fcf0707",
+    "url": "/react-expense-tracker/index.html"
+  },
+  {
+    "revision": "ec2d15f93afd19301ff4",
+    "url": "/react-expense-tracker/static/css/main.5c1a96d7.chunk.css"
+  },
+  {
+    "revision": "92110e239831f0bfb7b9",
+    "url": "/react-expense-tracker/static/js/2.c689ddfe.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-expense-tracker/static/js/2.c689ddfe.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ec2d15f93afd19301ff4",
+    "url": "/react-expense-tracker/static/js/main.88f2f8c7.chunk.js"
+  },
+  {
+    "revision": "241a7f1230ac62a2d81c",
+    "url": "/react-expense-tracker/static/js/runtime-main.aea2e40c.js"
+  }
+]);
